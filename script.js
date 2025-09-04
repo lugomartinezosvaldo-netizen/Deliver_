c1d77c3a0bf324a3d53424a7cb53281169b08314
lugomartinezosvaldo-netizen <lugomartinezosvaldo@gmail.com>
2025-09-04 01:42:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// script.js
+const verBtn = document.getElementById('verBtn');
+
+verBtn.addEventListener('click', () => {
+    window.location.href = "waka.html"; // Debe coincidir exactamente con el nombre del archivo
+});
